7892a6fa4055333c7c7a805a900be4d3878bbf1d
JinArima93 <nielpatrickneri@gmail.com>
2023-11-07 09:26:51 +0800

    categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import React, { useState } from 'react'
+import menu from './data'
+import Title from './Title'
+import Menu from './Menu'
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
+
 const App = () => {
-  return <h1>App</h1>
+  const [menuItem, setMenuItem] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='Mekus Mekus Menu' />
+        <Menu items={menuItem} />
+      </section>
+    </main>
+  )
 }
+
 export default App
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import MenuItem from './MenuItem'
 
-const Menu = () => {
-  return <h1>Menu</h1>
+const Menu = ({ items }) => {
+  return (
+    <div className='section-center'>
+      {items.map((item) => {
+        return <MenuItem {...item} key={item.id} />
+      })}
+    </div>
+  )
 }
 
 export default Menu
